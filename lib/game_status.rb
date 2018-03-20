9fc44ee2a37032c269b1ccc21ff2b6a40a2cc853
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-03-20 16:24:27 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,5 +1,9 @@
 def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+  if (board[index] == " " || board[index] == "") || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
 end
 
 WIN_COMBINATIONS = [
